a9adc07da97de9779b7d210985448a77410a7140
elvina-s <rammnuschka@gmail.com>
2023-12-12 16:57:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer, useRef, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function reducer(state, action) {
+  switch(action.type) {
+    case "START": return {...state, isTicking: true};
+    case "PAUSE": return {...state, isTicking: false};
+    case "RESET": return {clock: 0, isTicking: false};
+    case "TICK": return {...state, clock: state.clock+1}
+  }
+}
+
+const initialState = {
+  clock: 0,
+  isTicking: false,
+}
+
+function App () {
+const [state, dispatch] = useReducer(reducer, initialState);
+const myTimerRef = useRef(0);
+
+useEffect(() => {
+  if(!state.isTicking) {
+    return;
+  }
+  myTimerRef.current = setInterval(() => dispatch({type: "TICK"}), 1000);
+    return() => {
+      clearInterval(myTimerRef.current);
+      myTimerRef.current = 0
+    }
+}, [state.isTicking])
+
+
+  return (<div>
+    <p>{state.clock} seconds</p>
+    <button onClick={() => dispatch({type:"START"})}>START</button>
+    <button onClick={() => dispatch({type: "PAUSE"})}>PAUSE</button>
+    <button onClick={() => dispatch({type: "RESET"})}>RESET</button>
+  </div>
   );
 }
 
